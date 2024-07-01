db0ff2fb763236b2d04185d4fefc51065be1ab05
Evgeniy Gornakov <gornakovevgenij@yandex.ru>
2024-07-01 21:27:22 +0800

    first commit

---
+++ javaf/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("2+3");
+        System.out.println(2+3);
+
+
+        int  myMaxSpeed =17;
+        int x = speed + 3;
+
+        double d = 6.5;
+
+        String name = "Rep";
+
+        System.out.println(name + " exal co ckor" + speed );
+    }
+}
